ae56832443a8f41e8f632efc14478807f846728b
Monu-1989 <randhawamonu.1989@yahoo.com>
2025-07-28 03:22:57 +0530

    Create product_model.dart

---
+++ lib/product_model.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  final List<String> tags;
+
+  Product({
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    required this.tags,
+  });
+}
